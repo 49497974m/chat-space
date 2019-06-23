de79a2df9ae152b10347a9aacb428be5fb10d3de
49497974m <49497974tkhsm>
2019-06-23 13:56:43 +0900

    Update message.js

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    puts @group
-    puts @message
     if @message.save
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
